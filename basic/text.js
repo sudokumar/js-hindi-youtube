0e3ba528e3274589c5a6b6fb0dece3a8b2499afe
Kumar <kumarsaha875@gmail.com>
2025-07-23 19:36:17 +0000

    data type

--- basic/text.js
+++ basic/text.js
@@ -1,1 +1,35 @@
-console.log("hi")
+
+
+
+// // variable section
+// const accountId=1646646;// not change value (only one time define)
+// let accoundEmail= "kumar@gmail.com"  // scope maintained
+// var accountPasswor= "1234" // scope not maintained (prefer not to use var, bcz of issue in block scope and functional scope)  
+// accountCity="kaliyaganj"//varriable type menion not recomented in javaScript
+// let accountState;//  value ont difine (undifine)
+
+// console.log( accountId, accoundEmail, accountPasswor, accountCity) // it's normal print
+// console.table( [accountId, accoundEmail, accountPasswor, accountCity])// this (console.table) print all variable data table from
+
+
+
+
+
+
+"use strict"// treat all js code as newer version
+// alert (4+5) // we are using nodejs , not browser
+
+
+
+// // premitive dataType
+
+// number 
+// biginnt
+// String
+// Boolean
+// null  //( null is a object type data)
+// undefined // (this is a undifined data )
+// symbol
+
+console.log( typeof) // (cheak the type of data )
+
